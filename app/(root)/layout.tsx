a291975c757dff3fc95a6e98bca4bad6f4f7604b
Kulendran Pathmanathan <kulendranpathmanathan.97@gmail.com>
2024-05-15 07:51:10 +0530

    implement sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,18 @@
+import Sidebar from "@/components/ui/Sidebar";
+
 export const dynamic = "force-dynamic";
+
+const loggeduser = ["vinoth"];
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main>SIDEBAR{children}</main>;
+  return (
+    <main>
+      <Sidebar />
+      {children}
+    </main>
+  );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import HeaderBox from "@/components/ui/HeaderBox";
 
 const Home = () => {
-  return <div> Hello Home </div>;
+  const loggedIn = ["vinoth", "kule"];
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.[0] || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+        </header>{" "}
+      </div>
+    </section>
+  );
 };
 
 export default Home;
